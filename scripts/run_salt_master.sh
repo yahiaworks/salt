73a9123fb0fbdbfd4a1750a0b8f1ecfc4b911e4e
Melissa Michels <mmichels@vistaprint.com>
2017-12-05 10:11:53 -0500

    add saltapi user

--- scripts/run_salt_master.sh
+++ scripts/run_salt_master.sh
@@ -4,6 +4,10 @@ set -x
 
 ./replace_credentials.sh
 
+# Create local salt-api user
+RUN useradd -ms /bin/bash [SALTAPI_USERNAME]
+RUN echo '[SALTAPI_USERNAME]:[SALTAPI_PASSWORD]' | chpasswd
+
 salt-api -d
 salt-run winrepo.update_git_repos
 salt-master -c /etc/salt --log-file-level=quiet --log-level=info
