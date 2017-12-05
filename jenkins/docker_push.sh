1b20d8cadfad21f60d6abebb8fa7560f219a2393
Jonathan LaBroad <jlabroad@vistaprint.com>
2017-12-05 08:20:57 -0500

    Reorganizing a bit

--- jenkins/docker_push.sh
+++ jenkins/docker_push.sh
@@ -11,7 +11,20 @@ function publish {
     password=$4
 
     docker_login $repo $user $password
-    docker_push $repo $version
+    docker_tag_build_and_push $repo $version
+}
+
+function promote {
+    src_repo=$1
+    dst_repo=$2
+    version=$3
+    user=$4
+    password=$5
+
+    sudo docker tag "$src_repo/$image_name/$version" "$dst_repo/$image_name/$version"
+    
+    docker_login $dst_repo $user $password
+    docker_push "$dst_repo/$image_name/$version"
 }
 
 function docker_login {
@@ -22,17 +35,23 @@ function docker_login {
     sudo docker login $repo --username $user --password $password
 }
 
-function docker_push {
+function docker_tag_build_and_push {
     repo=$1
     version=$2
 
     image_full_name="$repo/$image_name"
 
     sudo docker tag salt-master $image_full_name:$version
-    sudo docker push $image_full_name:$version
+    docker_push $image_full_name:$version
 
     sudo docker tag salt-master $image_full_name:latest
-    sudo docker push $image_full_name:latest
+    docker_push $image_full_name:latest
+}
+
+function docker_push {
+    image=$1
+
+    sudo docker push $image
 }
 
 function cleanup {
